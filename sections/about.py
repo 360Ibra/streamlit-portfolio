5185ce1cffdacf16f20b95d20fd73bf610e2a885
Ibrahim Aminu <113380840+360Ibra@users.noreply.github.com>
2025-06-28 23:35:29 +0100

    About me modification - now using markup st.markup > st.write

--- sections/about.py
+++ sections/about.py
@@ -6,12 +6,19 @@ def show():
     st.title("Ibrahim Aminu")
     col1, col2 = st.columns([2, 1])
     with col1:
-        st.write(
-            "Hey! I’m Ibrahim, a determined software engineer passionate about building practical, impactful solutions. "
-            "My favorite language is Python, and I love exploring AI, machine learning, computer vision, and software development. "
-            "I currently work at Boston Scientific and am also pursuing my master’s in Computer Vision and AI.\n\n"
-            "Check out my projects, education, and contact info to learn more!"
+        st.markdown(
+            """
+            ### 👋 Hey! my name is Ibrahim ! 
+
+            I’m a **determined software engineer** passionate about building practical, impactful solutions.  
+            My favorite language is 🐍 **Python**, and I love exploring **AI**, **machine learning**, **computer vision**, and **software development**.
+
+            I currently work at **Boston Scientific** and I'm pursuing my master’s in **Computer Vision and AI** at **University Of Limerick**.
+
+            👉 Check out my [projects](#), [education](#), and [contact info](#) to learn more!
+            """
         )
+
 
     with col2:
         render_image("assets/profile_header.png", 300)
